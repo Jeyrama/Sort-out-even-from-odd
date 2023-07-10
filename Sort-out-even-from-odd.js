9f76e70a72e9d5cf8ad283cbc2571ee0cf6c658f
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-07-09 21:05:27 -0700

    Create Sort-out-even-from-odd.js

---
+++ Sort-out-even-from-odd.js
@@ -0,0 +1,18 @@
+/*
+Given an array/list [] of n integers, separate the even numbers from the odds.
+
+Notes:
+  Return an array/list where even numbers come first and then odds.
+  Then put even numbers in ascending order while odds are in descending order.
+  Array/list size is at least 4 with no zeroes.
+  Array/list numbers could be a mixture of positives and negatives.
+
+  Repetition of numbers in the array/list could occur, 
+  so (duplications are not included when separating).
+
+Example:
+  menFromBoys ({7, 3 , 14 , 17}) ==> return ({14, 17, 7, 3}) 
+  Explanation:
+    {14} is the even number here, so it came first, 
+    then the odds in descending order {17 , 7 , 3}.
+*/
